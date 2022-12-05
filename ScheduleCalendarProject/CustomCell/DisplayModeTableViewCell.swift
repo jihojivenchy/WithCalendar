aa811269fe2d6abcbf81d6feea0a632d7107999c
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2022-12-06 01:25:34 +0900

    Fifth Commited : Configure DisplayMode

---
+++ ScheduleCalendarProject/CustomCell/DisplayModeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DisplayModeTableViewCell.swift
+//  ScheduleCalendarProject
+//
+//  Created by 엄지호 on 2022/12/05.
+//
+
+import UIKit
+
+class DisplayModeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
