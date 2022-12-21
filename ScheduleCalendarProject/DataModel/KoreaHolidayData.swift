1c99ac5b803d954b6c3093f73a714eae3ef1f2fc
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2022-12-22 03:04:47 +0900

    Ninth Commited : Configure Get Lunar Date

--- ScheduleCalendarProject/DataModel/KoreaHolidayData.swift
+++
@@ -1,8 +0,0 @@
-//
-//  KoreaHolidayData.swift
-//  ScheduleCalendarProject
-//
-//  Created by 엄지호 on 2022/11/28.
-//
-
-import Foundation
---
+++ ScheduleCalendarProject/DataModel/XMLResponse.swift
@@ -0,0 +1,8 @@
+//
+//  XMLResponse.swift
+//  ScheduleCalendarProject
+//
+//  Created by 엄지호 on 2022/12/21.
+//
+
+import Foundation
