660e6b5ced8064c2fa034b0608ccdece5189ead5
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2022-12-23 19:14:27 +0900

    Tenth Commited : Configure Search Data and Color Set

---
+++ ScheduleCalendarProject/CustomCell/ColorSetCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  ColorSetCollectionViewCell.swift
+//  ScheduleCalendarProject
+//
+//  Created by 엄지호 on 2022/12/22.
+//
+
+import UIKit
+
+class ColorSetCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ ScheduleCalendarProject/DataModel/XMLKeys.swift
@@ -0,0 +1,8 @@
+//
+//  XMLKeys.swift
+//  ScheduleCalendarProject
+//
+//  Created by 엄지호 on 2022/12/22.
+//
+
+import Foundation
