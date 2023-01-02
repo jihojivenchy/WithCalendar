415bd2c4513756def37be28aa4cd184f6318e25b
엄지호 <eomjiho@eomjihoui-MacBookAir.local>
2023-01-02 14:48:35 +0900

    Configure Notification Detail

---
+++ ScheduleCalendarProject/MainViewControllers/WritingScheduleViewControllers/NotificationViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NotificationViewController.swift
+//  ScheduleCalendarProject
+//
+//  Created by 엄지호 on 2022/12/31.
+//
+
+import UIKit
+
+class NotificationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
