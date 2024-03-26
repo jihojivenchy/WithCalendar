76d5155e7f3876a78aac2151a8832c4a7d81449c
jihojivenchy <99619107+jihojivenchy@users.noreply.github.com>
2024-03-27 01:26:25 +0900

    feat: User 구현

---
+++ Sources/Model/User/User.swift
@@ -0,0 +1,15 @@
+//
+//  User.swift
+//  ScheduleCalendarProject
+//
+//  Created by 엄지호 on 3/27/24.
+//
+
+import Foundation
+
+struct User {
+    var uid: String
+    var name: String
+    var code: String
+    var email: String
+}
